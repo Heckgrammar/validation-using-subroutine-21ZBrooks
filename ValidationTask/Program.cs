3870b66e1b31cb2247238347206e3883a87af888
GreyShadow050 <21ZBrooks@heckgrammar.co.uk>
2024-09-18 13:49:04 +0100

    test

--- ValidationTask/Program.cs
+++ ValidationTask/Program.cs
@@ -6,7 +6,6 @@
         {
             string firstName;
             string lastName;
-            string username;
             string password;
             string emailAddress;
             int age;
@@ -25,55 +24,55 @@
             emailAddress = Console.ReadLine();
 
             
-            username = createUserName(firstName,lastName,age);
+            string username = createUserName(firstName,lastName,age);
             Console.WriteLine($"Username is {username}, you have successfully registered please remember your password");
 
-            //  Test your program with a range of tests to show all validation works
-            // Show your evidence in the Readme
+           // Test your program with a range of tests to show all validation works
+           //Show your evidence in the Readme
 
         }
-        static bool ValidName(string name)
-        {
-            
-        }
+        //static bool ValidName(string name)
+        //{
 
-        static bool validAge(int age)
-        {
-            //age must be between 11 and 18 inclusive
+        //}
 
-        }
+        //static bool validAge(int age)
+        //{
+        //    //age must be between 11 and 18 inclusive
 
-   
-        static bool ValidPassword(string password)
-        {
-            // Check password is at least 8 characters in length
+        //}
 
 
-            // Check password contains a mix of lower case, upper case and non letter characters
-            // QWErty%^& = valid
-            // QWERTYUi = not valid
-            // ab£$%^&* = not valid
-            // QWERTYu! = valid
+        //static bool ValidPassword(string password)
+        //{
+        //    // Check password is at least 8 characters in length
 
 
-            // Check password contains no runs of more than 2 consecutive or repeating letters or numbers
-            // AAbbdd!2 = valid (only 2 consecutive letters A and B and only 2 repeating of each)
-            // abC461*+ = not valid (abC are 3 consecutive letters)
-            // 987poiq! = not valid (987 are consecutive)
+        //    // Check password contains a mix of lower case, upper case and non letter characters
+        //    // QWErty%^& = valid
+        //    // QWERTYUi = not valid
+        //    // ab£$%^&* = not valid
+        //    // QWERTYu! = valid
+
+
+        //    // Check password contains no runs of more than 2 consecutive or repeating letters or numbers
+        //    // AAbbdd!2 = valid (only 2 consecutive letters A and B and only 2 repeating of each)
+        //    // abC461*+ = not valid (abC are 3 consecutive letters)
+        //    // 987poiq! = not valid (987 are consecutive)
 
 
 
-        }
-        static bool validEmail(string email)
-        {
-            // a valid email address
-            // has at least 2 characters followed by an @ symbol
-            // has at least 2 characters followed by a .
-            // has at least 2 characters after the .
-            // contains only one @ and any number of .
-            // does not contain any other non letter or number characters
+        //}
+        //static bool validEmail(string email)
+        //{
+        //    // a valid email address
+        //    // has at least 2 characters followed by an @ symbol
+        //    // has at least 2 characters followed by a .
+        //    // has at least 2 characters after the .
+        //    // contains only one @ and any number of .
+        //    // does not contain any other non letter or number characters
 
-        }
+        //}
         static string createUserName(string firstName, string lastName, int age)
         {
             // username is made up from:
@@ -81,10 +80,13 @@
             // last two characters of last name
             // age
             //e.g. Bob Smith aged 34 would have the username Both34
-
-
-
+            string userName="";
+            firstName=firstName.Substring(0,2);
+            userName = userName + firstName;
+            lastName = lastName.Substring(lastName.Length-3, lastName.Length-1);
+            userName = userName + lastName;
+            userName = userName + age;
+            return userName;
         }
-
     }
 }
